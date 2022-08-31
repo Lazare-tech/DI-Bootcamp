6a9e11b8bb96b83128edb47d5ed2bdf6be26b360
SOME yel-mani Lazare <70328538+Loliette@users.noreply.github.com>
2022-08-31 12:29:56 +0200

    Add files via upload

--- week_2/day_2/DaillyChallenge/Exercise.js
+++ week_2/day_2/DaillyChallenge/Exercise.js
@@ -1,9 +1,10 @@
 let sentence="The movie is not that bad, I like it";
 let wordNot=sentence.indexOf("not");
 let  wordBad=sentence.indexOf("bad");
-console.log(wordNot);
 if(wordBad>wordNot){
-    console.log("The movie is good,i like it");
+    let newSentence=console.log(sentence.replace("not that bad","good"));
+    console.log(newSentence);
+
 }
 else{
     console.log(sentence);
