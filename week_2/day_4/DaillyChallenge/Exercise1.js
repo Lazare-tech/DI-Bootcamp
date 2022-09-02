f954865434c76e9add53fec7fa43273d1adc5890
SOME yel-mani Lazare <70328538+Loliette@users.noreply.github.com>
2022-09-02 14:42:41 +0200

    Add files via upload

--- week_2/day_4/DaillyChallenge/Exercise1.js
+++ week_2/day_4/DaillyChallenge/Exercise1.js
@@ -1,7 +1,7 @@
 //: Words in the stars
 let chaine=prompt("Entrer une chaine de caractere separe par des virgules");
 let convert=chaine.split(",");
-let n="*",c=0,long=0,espace=" ";
+let n="*",c=0,long=0;
 console.log(convert);
  for (i=0; i<convert.length; i++){
       if (convert[i].length > long){
@@ -10,15 +10,23 @@ console.log(convert);
 
   }
 console.log(long);
-console.log(n.repeat(long));
+console.log(n.repeat(long+5));
 for(let i=0;i<convert.length;i++){
+  let marge=(long+1)-convert[i].length;
+  let espace=" ";
+
+  for(let i=0;i<marge;i++){
+    espace=espace+" ";
+    
+  }
 //   if(convert[i].length>convert[i+1].length){
-    console.log("* "+convert[i]+espace.repeat()+"*");
-//   }
+    console.log("* "+ convert[i] + espace+"*");
+}
+
 	
 
-}
-console.log(n.repeat(long+4));
+
+console.log(n.repeat(long+5));
 
 
 
