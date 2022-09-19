202fa402605e93ccd3b9937e1be568db162a9653
SOME yel-mani Lazare <70328538+Loliette@users.noreply.github.com>
2022-09-19 03:07:17 +0200

    Delete EXercise1.py

--- week_4/ExercisesXP/EXercise1.py
+++
@@ -1,9 +0,0 @@
-# Instructions
-
-#     Write a function called display_message() that prints one sentence telling everyone what you are learning in this course.
-#     Call the function, and make sure the message displays correctly.
-
-
-def display_message():
-    print("Dans ce cours nous avons appris le fonctionnment des fonctions")
-display_message()
