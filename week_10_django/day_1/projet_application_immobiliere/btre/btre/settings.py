310c8ce50c83b61d595c8e6e5b5a5224f45a7613
loliette <yelmaniyel@gmail.com>
2023-04-25 14:42:59 +0200

    home page,about page,listings page available

--- week_10_django/day_1/projet_application_immobiliere/btre/btre/settings.py
+++ week_10_django/day_1/projet_application_immobiliere/btre/btre/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.humanize',
     "pages",
     "listings",
     "realtors",
@@ -128,6 +129,8 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'btre/static'),
 ]
 
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- week_10_django/day_1/projet_application_immobiliere/btre/pages/views.py
+++ week_10_django/day_1/projet_application_immobiliere/btre/pages/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import HttpResponse
 from django.shortcuts import render,redirect
-
+from realtors.models import Realtor
+from listings.models import Listing
 # Create your views here.
 def index(request):
-    return render(request,'home.html')
+    listings=Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+ 
+    context={
+        'listings':listings
+    }
+    return render(request,'home.html',context)
 
 def about(request):
-    return render(request,'about.html')
+    #realtors
+    realtors= Realtor.objects.order_by('-hire_date')
+    #mvp
+    mvp_realtors= Realtor.objects.all().filter(is_mvp=True)
+    context={
+        'realtors':realtors,
+        'mvp_realtors':mvp_realtors,
+    }
+    return render(request,'about.html',context)
